605e53b2b6243eafdc46eb3dbdf917c0140e7599
Michal Zarzycki <michal.p.zarzycki@wp.pl>
2019-12-11 16:17:44 +0100

    Change units to si and language to pl

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
 const formButton = document.querySelector('#formButton');
 const inputForm = document.querySelector('#inputForm');
-const localTemp = document.querySelector('.localTemp')
+const localTemp = document.querySelector('.localTemp');
+const localSummary = document.querySelector('.localSummary');
 const APIKEY = 'zAWbqcYURiFGcj3d6FXEazB4OwbYGm0S';
 console.log(formButton);
 
 function fetchData(lat, long, apikey="a54a5cf8aa1c0dcd3e1272b77860a1c9") {
-   const cors = "https://cors-anywhere.herokuapp.com/"
+    const cors = "https://cors-anywhere.herokuapp.com/"
     const url = "https://api.darksky.net/forecast/";
-    let weather = [];
 
-    fetch(`${cors}${url}/${apikey}/${lat},${long}`)
+    fetch(`${cors}${url}/${apikey}/${lat},${long}?lang=pl&units=si`)
     .then(data => data.json())
     .then(data => {
-        console.log("data ", data);
         const { temperature, summary } = data.currently;
-        document.querySelector(".localTemp").textContent=`Temp: ${temperature}`
+        localTemp.textContent=`Temp: ${temperature}`;
+        localSummary.textContent=`Summary: ${summary}`
+
+
         
     });
-   return weather;
 }
 
 
@@ -27,16 +28,9 @@ window.addEventListener('load', (event) => {
     //Jeżeli zezwoliłeś na podanie swojej lokalizacji
     if(navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(position => {
-
         latitude = position.coords.latitude;
         longitude = position.coords.longitude;
         fetchData(latitude, longitude);
-
-     
-    
-
-     
-        
     })
 } else {
     //Jeżeli nie zgodziłeś się podaj pogodę w jakimś losowym miejscu na świecie(wybór losowy z tablicy 10 miejsc)
