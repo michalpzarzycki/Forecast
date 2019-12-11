b2e53a261c578b119c6642d044afec0065d45658
Michal Zarzycki <michal.p.zarzycki@wp.pl>
2019-12-11 13:44:52 +0100

    Added location latitude and lonitude to show forecast for user location

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const formButton = document.querySelector('#formButton');
 const inputForm = document.querySelector('#inputForm');
-const APIKEY = 'pX94AEzO31JtolUAiMOyrwkQH75igrAA';
+const APIKEY = 'zAWbqcYURiFGcj3d6FXEazB4OwbYGm0S';
 console.log(formButton);
 
 function fetchData() {
@@ -9,11 +9,28 @@ function fetchData() {
 
 
 formButton.addEventListener('click', (e)=>{
+    let lat;
+    let long;
+    if(navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(position => {
+          lat = position.coords.latitude;
+          long = position.coords.longitude;
+          console.log("lat: "+ lat + "long: " + long)
+      })
+    }
     console.log(inputForm.value);
     let cityName = inputForm.value;
     e.preventDefault();
-    let data = fetch(`http://dataservice.accuweather.com/locations/v1/cities/search?apikey=fcZ8fCiEInycKm7ePoM1yKWRAtpEvoCm&q=${cityName}`)
-    .then(data => data.json()).then(response => console.log(response)).catch(err => console.log(err))
+    let data = fetch(`http://api.accuweather.com/locations/v1/search?q=Bialogard&apikey=${APIKEY}`)
+    .then(data => data.json())
+    .then(
+        response => {
+            console.log(response)
+                    
+        }
+        
+        )    
+    .catch(err => console.log(err))
     
 
 })
